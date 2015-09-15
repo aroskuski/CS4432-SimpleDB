ee2aaa02f39edbed4c4c0deac2b5bd0dbfecb1fe
Kevin Zhao <kzhao2@wpi.edu>
2015-09-15 19:55:41 -0400

    Added the SQL file with our tables and queries

---
+++ Examples.sql
@@ -0,0 +1,25 @@
+/*Our queries can also be located in the SampleQueries.java file. Running this
+with the server creates the tables and runs our queries*/
+
+CREATE TABLE Gear (GName: Varchar(20) Primary Key, BrandId: INTEGER, MAbility: Varchar(20));
+
+CREATE TABLE Brand (Bid: INTEGER Primary Key, BName: Varchar(20), FavoredSub: Varchar(20));
+
+INSERT INTO Gear (GName, BrandId, MAbility) VALUES ('Gas Mask', 2, 'Tenacity'),
+					         ('Moto Boots', 1, 'Quick Respawn'),
+					         ('Cherry Kicks', 1, 'Stealth Jump'),
+					         ('Octo Tee', 3, 'Haunt'),
+					         ('Tinted Shades', 4, 'Last-Ditch Effort'),
+					         ('Basic Tee', 5, 'Quick Respawn');
+
+INSERT INTO Gear (Bid, BName, FavoredSub) VALUES (1, 'Rockenberg', 'Run Speed Up'),
+					         (2, 'Forge', 'Special Duration Up'),
+					         (3, 'Cuttlegear', 'NULL'),
+					         (4, 'Zekko', 'Special Saver'),
+					         (5, 'Squidforce', 'Damage Up'),
+					         (6, 'Inkline', 'Defense Up'),
+					         (7, 'Tentatek', 'Ink Recovery Up');
+
+SELECT GName, BName, FavoredSub
+FROM Gear G, Brand B
+WHERE G.BrandId = B.Bid
